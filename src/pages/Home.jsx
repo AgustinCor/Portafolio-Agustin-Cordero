703718c7e4ed95c79085c20ff0fa810a361e186e
AgustinCor <agustin.cordero@gmail.com>
2022-12-10 01:24:20 -0300

    new line

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,12 @@ const Home = () => {
     return (
         <div className='home-background'>
           <div className='icon-container'>
-            <i class="fa-brands fa-linkedin-in"></i>
-            <i class="fa-brands fa-github"></i>
+            <a target="_blank" href="https://www.linkedin.com/in/agust%C3%ADn-cordero-002557216/">
+              <i class="fa-brands fa-linkedin-in"></i>
+            </a>
+            <a target="_blank" href="https://github.com/AgustinCor">
+              <i class="fa-brands fa-github"></i>
+            </a>
           </div>
           <h1>Agustin Cordero</h1>
           <h2>Front-end developer</h2>
