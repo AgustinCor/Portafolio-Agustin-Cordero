749fb29f86a4d300bc9e086b18ecf2a6982c443e
AgustinCor <agustin.cordero@gmail.com>
2023-02-13 00:34:01 -0300

    skills update

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,9 +1,11 @@
 import pro1 from "./img/project1.png"
 import pro2 from "./img/project2.png"
 import pro3 from "./img/project3.png"
+import icon1 from "./img/postgresql.png"
 
 export default{
    "img1":pro1,
    "img2":pro2,
    "img3":pro3,
+   "postgreicon":icon1,
 }
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,59 +1,105 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import imagenes from "../assets/images";
 
 const Skills = () => {
   return (
     <div className="card-container">
-      <h2>My skills:</h2>
+      <h2></h2>
       <div>
-        <Card style={{ width: "18rem" }}>
+        <Card className="html-card" style={{ width: "18rem" }}>
           <Card.Body>
             <Card.Title>
-              <i className="fa-brands fa-html5"></i>
+              <i className="fa-solid fa-diamond"></i>
             </Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Html</Card.Subtitle>
-            <Card.Text className="html-color">
-    
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>
-              <i className="fa-brands fa-css3-alt"></i>
-            </Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">CSS</Card.Subtitle>
-            <Card.Text className="css-color">
-             
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>
-              <i className="fa-brands fa-js"></i>
-            </Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              Java Script
+            <Card.Subtitle className="mb-2 cart-sign">
+              <div>
+                <i className="fa-brands fa-html5"></i>
+              </div>
             </Card.Subtitle>
-            <Card.Text className="js-color">
-              
+
+            <Card.Text >
+              <i className="fa-solid fa-diamond"></i>
             </Card.Text>
           </Card.Body>
         </Card>
 
-        <Card style={{ width: "18rem" }}>
+        <Card className="css-card" style={{ width: "18rem" }}>
           <Card.Body>
             <Card.Title>
-              <i className="fa-brands fa-react"></i>
+              <i className="fa-solid fa-diamond"></i>
             </Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              React
+            <Card.Subtitle className="mb-2 cart-sign">
+              <div>
+                <i className="fa-brands fa-css3-alt"></i>
+              </div>
             </Card.Subtitle>
-            <Card.Text className="react-color">
-              
+            <Card.Text>
+              <i className="fa-solid fa-diamond"></i>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="js-card" style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>
+              <i className="fa-solid fa-diamond"></i>
+            </Card.Title>
+            <Card.Subtitle className="mb-2 cart-sign">
+              <div>
+                <i className="fa-brands fa-js"></i>
+              </div>
+            </Card.Subtitle>
+            <Card.Text >
+              <i className="fa-solid fa-diamond"></i>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="react-card" style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>
+              <i className="fa-solid fa-diamond"></i>
+            </Card.Title>
+            <Card.Subtitle className="mb-2 cart-sign">
+              <div>
+                <i className="fa-brands fa-react"></i>
+              </div>
+            </Card.Subtitle>
+            <Card.Text>
+              <i className="fa-solid fa-diamond"></i>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="node-js-card" style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>
+              <i class="fa-solid fa-diamond"></i>
+            </Card.Title>
+            <Card.Subtitle className="mb-2 cart-sign">
+              <div>
+                <i class="fa-brands fa-node-js"></i>
+              </div>
+            </Card.Subtitle>
+            <Card.Text>
+              <i className="fa-solid fa-diamond"></i>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="postgre-card" style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>
+              <i class="fa-solid fa-diamond"></i>
+            </Card.Title>
+            <Card.Subtitle className="mb-2 cart-sign">
+              <div>
+                <img src={imagenes.postgreicon} alt="" />
+              </div>
+            </Card.Subtitle>
+            <Card.Text>
+              <i class="fa-solid fa-diamond"></i>
             </Card.Text>
           </Card.Body>
         </Card>
