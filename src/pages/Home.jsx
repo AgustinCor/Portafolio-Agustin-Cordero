796c60b7a1f6b475d141214c11cbcc18c743961a
agustin cordero <agustin.cordero201@gmail.com>
2023-05-29 17:46:20 -0300

    background changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
 import CV from "../assets/Agustin Cordero CV.pdf";
 
 const Home = () => {
-    return (
-        <div className='home-background'>
-          <div className='icon-container'>
-            <a target="_blank" href="https://www.linkedin.com/in/agust%C3%ADn-cordero-002557216/">
-              <i className="fa-brands fa-linkedin-in"></i>
-            </a>
-            <a target="_blank" href="https://github.com/AgustinCor">
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a href={CV} download >
-              <i className='cv-icon' >CV</i>
-            </a>
+  return (
+    <div className="home-background">
+      <div className="icon-container">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/agust%C3%ADn-cordero-002557216/"
+        >
+          <i className="fa-brands fa-linkedin-in"></i>
+        </a>
+        <a target="_blank" href="https://github.com/AgustinCor">
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a href={CV} download>
+          <i className="cv-icon">CV</i>
+        </a>
+      </div>
+      <div class="cristalBall">
+        <div class="cristal">
+          <div class="ball">
+            <h1>Agustin Cordero</h1>
+            <h2>Front-end developer</h2>
           </div>
-          <h1>Agustin Cordero</h1>
-          <h2>Front-end developer</h2>
         </div>
-    );
+      </div>
+      <div class="background">
+        <div class="circle">
+          <div class="circle1">
+            <div class="circle2">
+              <div class="circle3"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
